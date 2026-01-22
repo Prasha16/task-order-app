0671071eccaebdba2d9a93d28f529834386e53a9
Prashanth Kumar Reddy GANGIREDDYGARI <prashanthg@MTLP-3621.local>
2026-01-22 16:11:10 +0530

    setup project for TaskOrderApp
    - Added App.tsx as entry point
    - Added project configuration files: babel, metro, tsconfig
    - Added package.json and lock files for dependency management
    - Added Gemfile and Gemfile.lock for Ruby dependencies

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { store } from './src/redux/store';
+import { AppNavigator } from './src/navigation/AppNavigator';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </Provider>
+  );
+};
+
+export default App;
